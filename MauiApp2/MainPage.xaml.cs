ab92932eeb89b9e2de9cc83d33e59656d34e1405
Samuel <samuelreyesiglesias@gmail.com>
2022-05-03 15:55:28 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public Command CounterClickCommand3 { get; set; }
+
+	public void testMethod()
+    {
+		App.Current.MainPage.DisplayAlert("testing 3 UI UX MAUI", "testing a message", "ok");
+
+	}
+
+	public void methodCommand()
+	{
+
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+		App.Current.MainPage.DisplayAlert("testing UI UX MAUI", "testing a message", "ok");
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext =  new MauiApp2.Services.ComandosPrueba();
+		//CounterClickCommand3  = new Command(testMethod);
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		this.methodCommand();
+	}
+
+	
+}
+
---
+++ MauiApp2/Services/ComandosPrueba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Services
+{
+    public  class ComandosPrueba
+    {
+
+		public Command CounterClickCommand { get; set; }
+
+		public ComandosPrueba()
+		{
+			this.CounterClickCommand = new Command(this.methodCommand);
+		}
+
+		public void methodCommand()
+		{ 
+			App.Current.MainPage.DisplayAlert("testing 2 UI UX MAUI", "testing a message", "ok"); 
+			
+		}
+	}
+}
